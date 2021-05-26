e4eb69d46a71bb6055ca01932299d0a00914225b
xiyang <ianyeungcanton@163.com>
2021-05-26 19:59:51 +0800

    update rename and remove file script

--- scripts/data_preparation/rename.py
+++ scripts/data_preparation/rename.py
@@ -6,11 +6,24 @@ def rename(root):
     img_paths = sorted(glob.glob(os.path.join(root, '*', '*', '*.png')))
     for ori_img_path in img_paths:
         dst_img_path = ori_img_path.replace('im1.png', 'im4.png')
-        command = 'mv {} {}'.format(ori_img_path, dst_img_path)
-        print(command)
-        os.system(command=command)
+        print('rename {} into {}'.format(ori_img_path, dst_img_path))
+        os.rename(ori_img_path, dst_img_path)
+
+
+def remove(root):
+    img_paths = sorted(glob.glob(os.path.join(root, '*', '*', 'im4.png')))
+    for img_path in img_paths:
+        print('remove {}'.format(img_path))
+        os.remove(img_path)
 
 
 if __name__ == '__main__':
-    root = '/data2/yangxi/datasets/Vimeo90k/vimeo_septuplet_BIx2_h264_crf23_com_crf28_img/sequences'
-    rename(root)
+    mode = 'crf'
+    # exp_name = f'001_MSRResNet_x2_f64b16_Vimeo90k_250k_B16G1_wandb'
+    # exp_name = f'001_MSRResNet_EncoderDecoder_x2_Vimeo90k_250k_crf23'
+    exp_name = f'001_MSRResNet_EncoderDecoder_x2_Vimeo90k_250k_crf28'
+    # exp_name = f'001_MSRResNet_BIC_x2_Vimeo90k_250k_crf23'
+    # exp_name = f'001_MSRResNet_BIC_x2_Vimeo90k_250k_crf28'
+    for quality in [18, 23, 28, 33]:
+        root = f'/home/xiyang/Datasets/ReCp/results/{exp_name}/visualization/Vimeo90k_{mode}{quality}_frame'
+        rename(root)
