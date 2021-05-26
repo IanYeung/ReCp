37813b201f1dc97d5dd119e017aa8fb9ebe657f5
xiyang <ianyeungcanton@163.com>
2021-05-26 19:59:17 +0800

    add LapPyrLoss

--- basicsr/losses/losses.py
+++ basicsr/losses/losses.py
@@ -447,3 +447,66 @@ def gradient_penalty_loss(discriminator, real_data, fake_data, weight=None):
         gradients_penalty /= torch.mean(weight)
 
     return gradients_penalty
+
+
+def build_gauss_kernel(size=5, sigma=1.0, n_channels=1, cuda=False):
+    if size % 2 != 1:
+        raise ValueError("kernel size must be uneven")
+    grid = np.float32(np.mgrid[0:size, 0:size].T)
+    gaussian = lambda x: np.exp((x - size // 2) ** 2 / (-2 * sigma ** 2)) ** 2
+    kernel = np.sum(gaussian(grid), axis=2)
+    kernel /= np.sum(kernel)
+    # repeat same kernel across depth dimension
+    kernel = np.tile(kernel, (n_channels, 1, 1))
+    # conv weight should be (out_channels, groups/in_channels, h, w),
+    # and since we have depth-separable convolution we want the groups dimension to be 1
+    kernel = torch.FloatTensor(kernel[:, None, :, :])
+    if cuda:
+        kernel = kernel.cuda()
+    kernel.requires_grad = False
+    return kernel
+
+
+def conv_gauss(img, kernel):
+    """ convolve img with a gaussian kernel that has been built with build_gauss_kernel """
+    n_channels, _, kw, kh = kernel.shape
+    img = F.pad(img, (kw // 2, kh // 2, kw // 2, kh // 2), mode='replicate')
+    return F.conv2d(img, kernel, groups=n_channels)
+
+
+def laplacian_pyramid(img, kernel, max_levels=5):
+    current = img
+    pyr = []
+
+    for level in range(max_levels):
+        filtered = conv_gauss(current, kernel)
+        diff = current - filtered
+        pyr.append(diff)
+        current = F.avg_pool2d(filtered, 2)
+
+    pyr.append(current)
+    return pyr
+
+
+@LOSS_REGISTRY.register()
+class LapLoss(nn.Module):
+    def __init__(self, max_levels=5, k_size=5, sigma=2.0):
+        super(LapLoss, self).__init__()
+        self.max_levels = max_levels
+        self.k_size = k_size
+        self.sigma = sigma
+        self._gauss_kernel = None
+
+    def forward(self, inp, tar):
+        if len(inp.shape) == 5:
+            B, N, C, H, W = inp.size()
+            inp = inp.view(-1, C, H, W)
+            tar = tar.view(-1, C, H, W)
+        if self._gauss_kernel is None or self._gauss_kernel.shape[1] != inp.shape[1]:
+            self._gauss_kernel = build_gauss_kernel(
+                size=self.k_size, sigma=self.sigma,
+                n_channels=inp.shape[1], cuda=inp.is_cuda
+            )
+        pyr_inp = laplacian_pyramid(inp, self._gauss_kernel, self.max_levels)
+        pyr_tar = laplacian_pyramid(tar, self._gauss_kernel, self.max_levels)
+        return sum(l1_loss(a, b) for a, b in zip(pyr_inp, pyr_tar))
