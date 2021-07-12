07b7dfb2cc392b4fdd28883ecef999d8ee3eaa25
xiyang <ianyeungcanton@163.com>
2021-07-12 09:35:52 +0800

    update arch testing code

--- tests/test_archs.py
+++ tests/test_archs.py
@@ -1,6 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
 
 from basicsr.archs import build_network
+from basicsr.utils.matlab_functions import bgr2ycbcr, ycbcr2bgr
 
 
 def get_encoderdecoder():
@@ -45,12 +50,47 @@ def get_fstrn():
     return net
 
 
+def get_condunet():
+    opt = {'type': 'CondUNet', 'in_nc': 3, 'out_nc': 3, 'nf': 64}
+    net = build_network(opt)
+    return net
+
+
+def get_compressor():
+    opt = {'type': 'FrameCompressor'}
+    net = build_network(opt)
+    return net
+
+
 if __name__ == '__main__':
 
-    device = torch.device('cuda:0')
+    device = torch.device('cpu')
 
     # inp = torch.randn(1, 3, 128, 224).to(device)
-    inp = torch.randn(1, 7, 3, 128, 224).to(device)
-    net = get_fstrn().to(device)
-    out = net(inp)
-    print(out.shape)
+    # # inp = torch.randn(1, 7, 3, 128, 224).to(device)
+    # net = get_condunet().to(device)
+    # out = net((inp, inp))
+    # print(out.shape)
+
+    net = get_compressor().to(device)
+
+    block_size = 4
+    img = cv2.imread('/home/xiyang/Downloads/0100/im1.png')
+    ori_img = bgr2ycbcr(img, y_only=True)  # [H, W]
+    plt.imshow(ori_img, cmap='gray')
+    plt.title('Original Image')
+    plt.show()
+
+    ori_images = torch.from_numpy(ori_img).unsqueeze(dim=0).unsqueeze(dim=0).float()  # [B, 1, H, W]
+
+    com_images, likelihoods = net(ori_images, qp=10)  # [B, 1, H, W]
+
+    com_img = (com_images.detach().squeeze(dim=0).permute(1, 2, 0).numpy()).round().astype(np.uint8).squeeze()  # [H, W]
+    plt.imshow(com_img, cmap='gray')
+    plt.title('Compressed Image')
+    plt.show()
+
+    print(np.mean(np.abs(com_img - ori_img)))
+    plt.imshow(np.abs(com_img - ori_img), cmap='gray')
+    plt.title('Difference Image')
+    plt.show()
